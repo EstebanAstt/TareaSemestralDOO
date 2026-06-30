c57e0230e6ed22c58ccea453d7cb486cc3de8025
soulsilver142 <joelrojas2025@udec.cl>
2026-06-29 22:56:24 -0400

    feat: agregar esqueleto de clase PosicionesGestion

--- src/gestion/PosicionesGestion.java
+++ src/gestion/PosicionesGestion.java
@@ -1,4 +1,23 @@
 package gestion;
 
+import modelo.Equipo;
+import modelo.MatchResultado;
+import modelo.disciplina.Disciplina;
+
 public class PosicionesGestion {
+    Disciplina disciplinaGestion;
+    MatchResultado resultado;
+    Equipo equipoUno;
+    Equipo equipoDos;
+
+    public void PosicionesGestion(Disciplina disciplinaGestion){
+        this.disciplinaGestion = disciplinaGestion;
+    }
+
+    public void procesarResultado(Equipo equipoUno, Equipo equipoDos, MatchResultado resultado){
+    }
+
+    public int getPuntaje(Equipo equipo){
+        return 0;
+    }
 }
