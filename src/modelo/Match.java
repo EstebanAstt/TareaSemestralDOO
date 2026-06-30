e5906f47887db18ce9b9a6891a8ab0346fe97b0f
soulsilver142 <joelrojas2025@udec.cl>
2026-06-30 13:19:56 -0400

    feat: agregar atributos y constructores a clase Match

--- src/modelo/Match.java
+++ src/modelo/Match.java
@@ -1,4 +1,46 @@
 package modelo;
 
+import java.time.LocalDateTime;
+
 public class Match {
+    private Participante participanteUno;
+    private Participante participanteDos;
+
+    /** Se registra el resultado en esta variable */
+    private MatchResultado resultado;
+
+    /** Fecha del enfrentamiento */
+    private LocalDateTime fecha;
+
+    private int ronda;
+    private int posicionBracket;
+
+    /** Constructor sin posición en bracket, sirve para el formato de liga */
+    public Match(Participante participanteUno, Participante participanteDos, int ronda){
+        this.participanteUno = participanteUno;
+        this.participanteDos = participanteDos;
+        this.ronda = ronda;
+    }
+
+    /** Constructor con posición en bracket, sirve para eliminatorias */
+    public Match(Participante participanteUno, Participante participanteDos,
+                 int ronda, int posicionBracket, LocalDateTime fecha){
+
+        /** Desde aquí se lanzan excepciones */
+        if (participanteUno == null || participanteDos == null){
+            throw new IllegalArgumentException();
+        }
+        if (participanteUno.equals(participanteDos)){
+            throw new IllegalArgumentException();
+        }
+        if (ronda < 1){
+            throw new IllegalArgumentException();
+        }
+
+        this.participanteUno = participanteUno;
+        this.participanteDos = participanteDos;
+        this.ronda = ronda;
+        this.posicionBracket = posicionBracket;
+        this.fecha = fecha;
+    }
 }
