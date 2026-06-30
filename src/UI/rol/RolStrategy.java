1ba25d31b0bf30501a5ae0402d8d2d467f9c1847
EstebanAstt <esteban.astetecif@gmail.com>
2026-06-30 19:43:22 -0400

    feat: Ahora para ejecutarOpcion se utiliza un Panel en lugar de un frame

--- src/UI/rol/RolStrategy.java
+++ src/UI/rol/RolStrategy.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public interface RolStrategy {
     String[] getOpciones();           // qué botones mostrar
-    void ejecutarOpcion(String opcion, JFrame ventanaActual); // qué hacer al presionar cada uno
+    void ejecutarOpcion(String opcion, JPanel actualPanel); // qué hacer al presionar cada uno
     String getNombreRol();
 
 }
