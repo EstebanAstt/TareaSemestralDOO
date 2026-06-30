2d2358b21eaf9d394a290f234f3080bbc14c325a
EstebanAstt <esteban.astetecif@gmail.com>
2026-06-30 19:54:37 -0400

    feat: ahora se llama a la AppWindow en vez del menuMain

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
-import UI.MenuMain;
+import UI.MenuMainPanel;
+import UI.Resources.AppWindow;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Usar el look and feel del sistema operativo
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            // Si falla, usa el look por defecto
-        }
-
-        SwingUtilities.invokeLater(() -> {
-            MenuMain window = new MenuMain();
-            window.setVisible(true);
-        });
+        SwingUtilities.invokeLater(() -> new AppWindow().setVisible(true));
     }
 }
