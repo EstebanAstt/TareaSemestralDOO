5254b8d64be9524af3b62b9fe3a1e27778b11cb2
EstebanAstt <esteban.astetecif@gmail.com>
2026-06-29 22:04:25 -0400

    feat: se puede inicializar la pantalla inicial

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
+import UI.MenuMain;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        // Usar el look and feel del sistema operativo
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            // Si falla, usa el look por defecto
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            MenuMain window = new MenuMain();
+            window.setVisible(true);
+        });
     }
 }
