426480a68e454ddc5d1c072aded14ca6d10f8287
soulsilver142 <joelrojas2025@udec.cl>
2026-07-01 19:32:15 -0400

    feat: modificar esqueleto de clase Torneo

--- src/modelo/Torneo.java
+++ src/modelo/Torneo.java
@@ -1,17 +1,30 @@
 package modelo;
 
+import java.util.ArrayList;
+import modelo.disciplina.Disciplina;
+import modelo.formato.TorneoFormato;
 import java.time.*;
 
 /**
- * Representa el torneo.
- * Este debe tener un nombre y una fecha establecida
+ * Representa el torneo
+ * Este debe tener un formato, disciplina y fecha de inicio
  */
 public class Torneo {
-    String nombreTorneo;
-    LocalDate fecha;
+    private String nombre;
+    private Disciplina disciplina;
+    private TorneoFormato formato;
+    private LocalDate fechaInicio;
 
-    public Torneo(String nombreTorneo, LocalDate fecha){
-        this.nombreTorneo = nombreTorneo;
-        this.fecha = fecha;
+    public Torneo(String nombre, Disciplina disciplina, TorneoFormato formato, LocalDate fechaInicio){
+        if (nombre == null || nombre.isBlank()){
+            throw new IllegalArgumentException("El torneo debe tener un nombre");
+        }
+        if (disciplina == null || formato == null){
+            throw new IllegalArgumentException("El torneo debe tener disciplina y un formato");
+        }
+        this.nombre = nombre;
+        this.disciplina = disciplina;
+        this.formato = formato;
+        this.fechaInicio = fechaInicio;
     }
 }
