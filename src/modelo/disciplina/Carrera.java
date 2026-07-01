cc339ecb6129740f52d19cde379e715ba3611c3e
soulsilver142 <joelrojas2025@udec.cl>
2026-07-01 01:14:51 -0400

    feat: implementar por patrón Strategy los métodos de Disciplina a clase Carrera

--- src/modelo/disciplina/Carrera.java
+++ src/modelo/disciplina/Carrera.java
@@ -1,26 +1,56 @@
 package modelo.disciplina;
 
-import modelo.Torneo;
+import modelo.MatchResultado;
 import modelo.formato.TorneoFormato;
+import java.util.ArrayList;
 
 public class Carrera implements Disciplina {
-    Torneo torneoCarrera;
-    TorneoFormato formatoCarrera;
-    int puntajesCarrera;
-
     @Override
-    public void setFormato(TorneoFormato formatoCarrera){
-        this.formatoCarrera = formatoCarrera;
+    public double puntajeVictoria(){
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    public int getPuntajes(Torneo torneo){
-        torneoCarrera = torneo;
-        return puntajesCarrera;
+    public double puntajeEmpate(){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public double puntajePerdida(){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean validarFormato(TorneoFormato formatoFutbol){
+        /** Este tipo de disciplina no ocupa ningún tipo de formato */
+        return false;
     }
 
     @Override
     public boolean permiteEmpate(){
         return false;
+    }
+
+    @Override
+    public String getNombreDisciplina(){
+        return "Carrera";
+    }
+
+    @Override
+    public boolean validarMatchResultado(MatchResultado resultado){
+        /** Los resultados no se registran como MatchResultado
+         *  en cambio, se registran los tiempos de llegada */
+        return false;
+    }
+
+    @Override
+    public ArrayList<CriterioDesempate> ordenCriteriosDesempate(){
+        ArrayList<CriterioDesempate> orden = new ArrayList<CriterioDesempate>();
+        return orden;
+    }
+
+    /** Determina que la disciplina ocupa un formato de tiempo cronometrado */
+    public boolean esCronometrada() {
+        return true;
     }
 }
