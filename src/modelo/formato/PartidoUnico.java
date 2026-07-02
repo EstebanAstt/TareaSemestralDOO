96a47b49dc1496212e1119cc8ae624b4ecd85d36
soulsilver142 <joelrojas2025@udec.cl>
2026-07-01 20:19:20 -0400

    feat: añadir métodos nuevos a interfaz TorneoFormato y por patrón Strategy agregarlos a PartidoUnico

--- src/modelo/formato/PartidoUnico.java
+++ src/modelo/formato/PartidoUnico.java
@@ -1,15 +1,38 @@
 package modelo.formato;
 
+import java.util.ArrayList;
 import modelo.Equipo;
+import modelo.Match;
 
 public class PartidoUnico implements TorneoFormato {
     @Override
-    public void setTorneoFormato(Equipo equipoTorneo){
-        System.out.println("Formato Generado: Partido Único");
+    public int getMinimoParticipantes(){
+        return 2;
+    }
+
+    @Override
+    public ArrayList<Match> generarMatches(Equipo equipoTorneo){
+        /** Hasta ahora se prueba un ArrayList de prueba, posteriormente
+         *  se debe cambiar el cálculo de la cantidad de matches a BracketsGestion */
+
+        // Revisar gestión después de los formatos
+
+        ArrayList<Match> matchesPartidoUnico = new ArrayList<>();
+
+        if (equipoTorneo.getEquipoSize() < getMinimoParticipantes()) {
+            throw new IllegalArgumentException("Se necesitan por lo menos 2 jugadores en un equipo");
+        }
+        return matchesPartidoUnico;
     }
 
     @Override
     public String getNombreFormato(){
         return "Partido Único";
+    }
+
+    @Override
+    public int getCantidadMatches(Equipo equipoTorneo){
+        int cantidadParticipantes = equipoTorneo.getEquipoSize();
+        return cantidadParticipantes - 1;
     }
 }
--- src/modelo/formato/TorneoFormato.java
+++ src/modelo/formato/TorneoFormato.java
@@ -1,8 +1,19 @@
 package modelo.formato;
 
+import java.util.ArrayList;
 import modelo.Equipo;
+import modelo.Match;
 
 public interface TorneoFormato {
-    void setTorneoFormato(Equipo equipoTorneo);
+
+    /** Retorna la cantidad mínima de participantes del formato */
+    int getMinimoParticipantes();
+
+    /** Genera enfrentamientos a partir de un equipo ingresado */
+    ArrayList<Match> generarMatches(Equipo equipoTorneo);
+
     String getNombreFormato();
+
+    /** Retorna la cantidad de enfrentamientos dependiendo la cantidad de jugadores */
+    int getCantidadMatches(Equipo equipoTorneo);
 }
