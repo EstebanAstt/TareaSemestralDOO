ea3d3ddab892be8433e14b46b52402ef71da8562
soulsilver142 <joelrojas2025@udec.cl>
2026-07-03 16:37:58 -0400

    feat: actualizar TorneoGestion con un método para crear un torneo

--- src/gestion/TorneoGestion.java
+++ src/gestion/TorneoGestion.java
@@ -1,38 +1,44 @@
 package gestion;
 
-import modelo.Equipo;
+import java.time.LocalDate;
 import modelo.Torneo;
 import modelo.disciplina.Disciplina;
 import modelo.formato.TorneoFormato;
 
-import java.util.ArrayList;
-
 public class TorneoGestion {
-    Torneo torneo;
-    Disciplina disciplina;
-    TorneoFormato formato;
-    ArrayList<Equipo> listaEquipos;
+    private Torneo torneo;
 
-    /** Recibe un torneo para después añadirle sus atributos */
-    public TorneoGestion(Torneo torneo){
-        this.torneo = torneo;
+    public TorneoGestion(){
     }
 
-    public void setDisciplina(Disciplina disciplina){
-        this.disciplina = disciplina;
+    /** Crea un torneo */
+    public Torneo crearTorneo(String nombre, Disciplina disciplina,
+                              TorneoFormato formato, LocalDate fechaInicio){
+        try {
+            this.torneo = new Torneo(nombre, disciplina, formato, fechaInicio);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Faltó ingresar un atributo");
+        }
+        return this.torneo;
     }
 
-    public void setFormatoTorneo(TorneoFormato formato){
-        this.formato = formato;
+    /** Verifica que la variable torneo no sea nula */
+    public void verificarTorneoActivo(){
+        if (torneo == null) {
+            throw new IllegalStateException("No existe un torneo activo");
+        }
     }
 
+    /*
     public void registrarEquipos(Equipo equipoIngresado){
         if (equipoIngresado == null) {
             throw new IllegalArgumentException();
         }
-
         if (!listaEquipos.contains(equipoIngresado)) {
             listaEquipos.add(equipoIngresado);
         }
     }
+    */
+
+    /** Aquí luego se crean métodos que replican las opciones del organizador*/
 }
