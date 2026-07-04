10b2de2d40ed9b40a1503ac6149a83e19fdcc19e
EstebanAstt <esteban.astetecif@gmail.com>
2026-07-04 03:51:43 -0400

    feat: se añaden nuevos colores para los botones de los Jpanels

--- src/UI/Resources/ColorPalette.java
+++ src/UI/Resources/ColorPalette.java
@@ -5,10 +5,18 @@ import java.awt.*;
 public enum ColorPalette {
     COLOR_BTN(new Color(56, 162, 234, 255)),
     COLOR_BTN_HOVER(new Color(27, 84, 120)),
-    COLOR_TEXT_DARK (new Color(37, 32, 2, 255)),
+    COLOR_BTN_DEACTIVATED(new Color(181, 177, 177, 255)),
+
     COLOR_BTN_BACK(new Color(150, 150, 150, 220)),
     COLOR_BTN_BACK_HOVER(new Color(110, 110, 110, 255)),
-    COLOR_TEXT_LIGHT(new Color(255, 255, 255, 255));
+
+    COLOR_BTN_DEL(new Color(220, 80, 80)),
+    COLOR_BTN_DEL_HOVER(new Color(180, 40, 40)),
+
+    COLOR_TEXT_DARK (new Color(37, 32, 2, 255)),
+    COLOR_TEXT_LIGHT(new Color(255, 255, 255, 255)),
+
+    COLOR_BG_LIGHT(new Color(255, 255, 255, 200));
 
     private final Color color;
 
