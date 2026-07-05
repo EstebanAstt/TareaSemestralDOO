f8a15e89fc92c5d35d63d2e4e0542942af53e55e
soulsilver142 <joelrojas2025@udec.cl>
2026-07-05 17:45:18 -0400

    feat: añadir métodos iniciales de observador a clase BracketsGestion

--- src/gestion/BracketsGestion.java
+++ src/gestion/BracketsGestion.java
@@ -1,4 +1,39 @@
 package gestion;
 
+import java.util.ArrayList;
+import modelo.Match;
+import modelo.formato.TorneoFormato;
+
+/** Hacer esta ahora */
 public class BracketsGestion {
+    private TorneoFormato formato;
+    private ArrayList<Match> partidos;
+
+    /** Lista de observadores del bracket, avisan actualizaciones */
+    private ArrayList<EstadoBracketsGestion> observadoresBracket;
+
+    public BracketsGestion(TorneoFormato formato){
+        this.formato = formato;
+        this.partidos = new ArrayList<>();
+        this.observadoresBracket = new ArrayList<>();
+
+        /** Se referencia que la gestión del bracket está aquí */
+        this.formato.setBracketsGestion(this);
+    }
+
+    /** Abajo hay métodos de observadores */
+
+    public void agregarObservadorBracket(EstadoBracketsGestion observador){
+        if (observador == null){
+            throw new IllegalArgumentException("El observador no puede ser nulo");
+        }
+        if (observadoresBracket.contains(observador)){
+            throw new IllegalArgumentException("Éste observador ya está en la lista de observadores");
+        }
+        observadoresBracket.add(observador);
+    }
+
+    public void eliminarObservadorBracket(EstadoBracketsGestion observador){
+        observadoresBracket.remove(observador);
+    }
 }
