ac8ea9fd478a6ce565e565015a9d0d38ab1a97c8
soulsilver142 <joelrojas2025@udec.cl>
2026-07-06 15:25:27 -0400

    test: añadir algunos test a TorneoGestionTest

--- src/test/TorneoGestionTest.java
+++ src/test/TorneoGestionTest.java
@@ -1,27 +1,76 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
-import gestion.TorneoGestion;
-import modelo.Torneo;
-import modelo.disciplina.Disciplina;
-import modelo.formato.TorneoFormato;
+import gestion.*;
+import modelo.disciplina.*;
+import modelo.formato.*;
+import modelo.*;
 import java.time.LocalDate;
 
 class TorneoGestionTest {
-    private Torneo torneo;
     private TorneoGestion torneoGestion;
+    private LocalDate fechaInicio;
+
     private String nombre;
     private Disciplina disciplina;
     private TorneoFormato formato;
-    private LocalDate fechaInicio;
+
+    private Participante participante1;
+    private Participante participante2;
+    private Equipo equipo;
 
     @BeforeEach
     void setUp(){
+        torneoGestion = new TorneoGestion();
+        fechaInicio = LocalDate.now();
+
+        participante1 = new Jugador("Pedro", "pedro@gmail.com");
+        participante2 = new Jugador("Marta", "marta@gmail.com");
+
+        equipo = new Equipo("Equipo A");
+        equipo.addjugador(participante1);
+        equipo.addjugador(participante2);
+
+        /** Se crea un torneo de ajedrez de prueba, con un formato válido */
+        nombre = "Torneo Ajedrez Test";
+        disciplina = new Ajedrez();
+        formato = new PartidoUnico();
     }
 
     @Test
     void TorneoInvalido(){
         assertThrows(IllegalArgumentException.class, ()->
-            torneoGestion.crearTorneo(nombre, disciplina, formato, fechaInicio),
-            "Debería lanzar IllegalArgumentException");
+                        torneoGestion.crearTorneo(null, disciplina, formato, fechaInicio),
+                "El torneo debe tener nombre");
+
+        assertThrows(IllegalArgumentException.class, ()->
+                        torneoGestion.crearTorneo(nombre, null, formato, fechaInicio),
+                "El torneo debe tener disciplina");
+
+        assertThrows(IllegalArgumentException.class, ()->
+                        torneoGestion.crearTorneo(nombre, disciplina, null, fechaInicio),
+                "El torneo debe tener formato");
+
+        assertThrows(IllegalArgumentException.class, ()->
+                        torneoGestion.crearTorneo(nombre, disciplina, formato, null),
+                "El torneo debe tener fecha de inicio");
+    }
+
+    @Test
+    void crearTorneoExitoso(){
+        Torneo torneoExitoso = torneoGestion.crearTorneo(nombre, disciplina, formato,
+                fechaInicio);
+
+        assertNotNull(torneoExitoso);
+        assertEquals(nombre, torneoExitoso.getNombreTorneo());
+        assertEquals(disciplina, torneoExitoso.getDisciplinaTorneo());
+        assertEquals(formato, torneoExitoso.getFormatoTorneo());
+        assertEquals(fechaInicio, torneoExitoso.getFechaInicioTorneo());
+    }
+
+    @Test
+    void verificarTorneoActivoSinTorneo(){
+        assertThrows(IllegalStateException.class, () ->
+                torneoGestion.verificarTorneoActivo(),
+                "No hay torneo activo");
     }
 }
