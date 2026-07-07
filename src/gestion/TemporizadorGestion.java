441d29eabc922d41980b46b18f6962582c0db6a6
soulsilver142 <joelrojas2025@udec.cl>
2026-07-06 23:47:13 -0400

    feat: implementar esqueleto de TemporizadorGestion para posteriormente ser ocupada en la disciplina de carrera

--- src/gestion/TemporizadorGestion.java
+++ src/gestion/TemporizadorGestion.java
@@ -1,4 +1,59 @@
 package gestion;
 
+import java.util.ArrayList;
+import gestion.temporizador.AsignacionTecla;
+import gestion.temporizador.ParticipanteTiempoLlegada;
+import modelo.Participante;
+
 public class TemporizadorGestion {
+    private long tiempoInicio;
+    private boolean estaCorriendo;
+
+    /** Lista con los participantes con su tecla respectiva */
+    private ArrayList<AsignacionTecla> participantesConTecla;
+
+    /** Tiempos de llegada de cada participante */
+    private ArrayList<ParticipanteTiempoLlegada> participantesTiemposLlegada;
+
+    public TemporizadorGestion(){
+        this.estaCorriendo = false;
+        this.participantesConTecla = new ArrayList<>();
+        this.participantesTiemposLlegada = new ArrayList<>();
+    }
+
+    public void empezarCarrera(){
+        this.tiempoInicio = System.nanoTime();
+        this.participantesTiemposLlegada.clear();
+        this.estaCorriendo = true;
+    }
+
+    public void pararCarrera(){
+        this.estaCorriendo = false;
+    }
+
+    public void asignarTecla(char tecla, Participante participante){
+        char teclaMayuscula = Character.toUpperCase(tecla);
+
+        // Por participante en "participantesConTecla"
+        for (AsignacionTecla pa : participantesConTecla){
+            if (pa.getTeclaAsignacionTecla() == teclaMayuscula){
+                throw new IllegalArgumentException("Esta tecla ya fué asignada a alguien");
+            }
+        }
+        AsignacionTecla participanteLocal = new AsignacionTecla(tecla, participante);
+        participantesConTecla.add(participanteLocal);
+    }
+
+    /**
+     * Revisa si el jugador ingresado llegó a la meta o no
+     */
+    public boolean jugadorLlegoMeta(Participante participante){
+        for (ParticipanteTiempoLlegada ptl : participantesTiemposLlegada){
+            Participante participanteLocal = ptl.getParticipanteTiempoLlegada();
+            if (participanteLocal.equals(participante)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
