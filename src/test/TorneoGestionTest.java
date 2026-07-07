c6e2cd4ca70ed5d0918707e6df0da2cfb47a7a36
soulsilver142 <joelrojas2025@udec.cl>
2026-07-06 20:34:45 -0400

    test: añadir tests de eliminación de equipos y participante, y además de generación de calendario

--- src/test/TorneoGestionTest.java
+++ src/test/TorneoGestionTest.java
@@ -5,15 +5,18 @@ import modelo.disciplina.*;
 import modelo.formato.*;
 import modelo.*;
 import java.time.LocalDate;
+import java.util.List;
 
 class TorneoGestionTest {
     private TorneoGestion torneoGestion;
-    private LocalDate fechaInicio;
 
+    /** Atributos del torneo */
     private String nombre;
     private Disciplina disciplina;
     private TorneoFormato formato;
+    private LocalDate fechaInicio;
 
+    /** Jugadores y un equipo */
     private Participante participante1;
     private Participante participante2;
     private Equipo equipo;
@@ -36,35 +39,32 @@ class TorneoGestionTest {
         formato = new IdaVuelta();
     }
 
-    @Test
-    void TorneoInvalido(){
-        assertThrows(IllegalArgumentException.class, ()->
-                        torneoGestion.crearTorneo(null, disciplina, formato, fechaInicio),
-                "El torneo debe tener nombre");
-
-        assertThrows(IllegalArgumentException.class, ()->
-                        torneoGestion.crearTorneo(nombre, null, formato, fechaInicio),
-                "El torneo debe tener disciplina");
-
-        assertThrows(IllegalArgumentException.class, ()->
-                        torneoGestion.crearTorneo(nombre, disciplina, null, fechaInicio),
-                "El torneo debe tener formato");
-
-        assertThrows(IllegalArgumentException.class, ()->
-                        torneoGestion.crearTorneo(nombre, disciplina, formato, null),
-                "El torneo debe tener fecha de inicio");
-    }
-
+    /** Torneo exitoso */
     @Test
     void crearTorneoExitoso(){
         Torneo torneoExitoso = torneoGestion.crearTorneo(nombre, disciplina, formato,
                 fechaInicio);
-
         assertNotNull(torneoExitoso);
         assertEquals(nombre, torneoExitoso.getNombreTorneo());
         assertEquals(disciplina, torneoExitoso.getDisciplinaTorneo());
         assertEquals(formato, torneoExitoso.getFormatoTorneo());
         assertEquals(fechaInicio, torneoExitoso.getFechaInicioTorneo());
+    }
+
+    /** Tests de creación de torneo inválido */
+    @Test
+    void crearTorneoInvalido() {
+        assertThrows(IllegalArgumentException.class, () ->
+                        torneoGestion.crearTorneo(null, disciplina, formato, fechaInicio),
+                "El torneo debe tener nombre");
+
+        assertThrows(IllegalArgumentException.class, () ->
+                        torneoGestion.crearTorneo(nombre, null, formato, fechaInicio),
+                "El torneo debe tener disciplina");
+
+        assertThrows(IllegalArgumentException.class, () ->
+                        torneoGestion.crearTorneo(nombre, disciplina, null, fechaInicio),
+                "El torneo debe tener formato");
     }
 
     @Test
@@ -73,26 +73,11 @@ class TorneoGestionTest {
         Torneo torneoFormatoInvalido = torneoGestion.crearTorneo(nombre,
                 disciplina, formatoInvalido, fechaInicio);
 
-        /** Cambiar esto a que la clase Torneo valide el formato
-         *  en su constructor */
         assertFalse(torneoFormatoInvalido.getDisciplinaTorneo().validarFormato(formatoInvalido),
                 "El formato ingresado no es válido");
     }
 
-    @Test
-    void verificarTorneoActivoSinTorneo(){
-        assertThrows(IllegalStateException.class, () ->
-                torneoGestion.verificarTorneoActivo(),
-                "No hay torneo activo");
-    }
-
-    @Test
-    void inscribirParticipanteGestionSinTorneo(){
-        assertThrows(IllegalStateException.class, () ->
-                torneoGestion.inscribirParticipanteGestion(participante1),
-                "Se inscribió un participante en un torneo que no existe");
-    }
-
+    /** Tests de inscripciones duplicadas */
     @Test
     void inscribirParticipanteGestionTorneoDuplicado(){
         torneoGestion.crearTorneo("Torneo Ajedrez Nuevo", disciplina,
@@ -102,5 +87,85 @@ class TorneoGestionTest {
         assertThrows(IllegalArgumentException.class, () ->
             torneoGestion.inscribirParticipanteGestion(participante1),
                 "Se inscribió el mismo participante dos veces");
+    }
+
+    @Test
+    void inscribirEquipoGestionTorneoDuplicado(){
+        torneoGestion.crearTorneo("Torneo Ajedrez Nuevo", disciplina,
+                formato, fechaInicio);
+        torneoGestion.inscribirEquipoGestion(equipo);
+
+        assertThrows(IllegalArgumentException.class, () ->
+                torneoGestion.inscribirEquipoGestion(equipo),
+                "Se inscibió el mismo equipo dos veces");
+    }
+
+    /** Tests de inscripción y eliminación sin torneo activo */
+    @Test
+    void inscribirParticipanteGestionSinTorneo(){
+        assertThrows(IllegalStateException.class, () ->
+                        torneoGestion.inscribirParticipanteGestion(participante1),
+                "Se inscribió un participante en un torneo que no existe");
+    }
+
+    @Test
+    void verificarTorneoActivoSinTorneo(){
+        assertThrows(IllegalStateException.class, () ->
+                        torneoGestion.verificarTorneoActivo(),
+                "No hay torneo activo");
+    }
+
+    @Test
+    void inscribirEquipoGestionSinTorneo(){
+        assertThrows(IllegalStateException.class, () ->
+                        torneoGestion.inscribirEquipoGestion(equipo),
+                "Se inscibió un equipo a un torneo que no existe");
+    }
+
+    @Test
+    void eliminarParticipanteGestionSinTorneo(){
+        assertThrows(IllegalStateException.class, () ->
+                torneoGestion.eliminarParticipanteGestion(participante1),
+                "Se eliminó un participante a un torneo que no existe");
+    }
+
+    @Test
+    void eliminarEquipoGestionSinTorneo(){
+        assertThrows(IllegalStateException.class, () ->
+                torneoGestion.eliminarEquipoGestion(equipo),
+                "Se eliminó un equipo a un torneo que no existe");
+    }
+
+    /** Tests de generación de calendario */
+    @Test
+    void generarCalendarioLigaExitoso() {
+        torneoGestion.crearTorneo(nombre, disciplina, formato, fechaInicio);
+
+        // Se agregan 4 participantes
+        for (int p = 0; p < 4; p++) {
+            Participante participanteLocal = new Jugador("Local");
+            torneoGestion.inscribirParticipanteGestion(participanteLocal);
+        }
+
+        List<JornadaGestion> jornadas = torneoGestion.generarCalendarioLiga();
+        assertNotNull(jornadas);
+        assertFalse(jornadas.isEmpty());
+        assertEquals(6, jornadas.size());
+    }
+
+    @Test
+    void generarCalendarioLigaMenosDeDosParticipantes(){
+        torneoGestion.inscribirParticipanteGestion(participante1);
+
+        assertThrows(IllegalStateException.class, () ->
+                torneoGestion.generarCalendarioLiga(),
+                "Hay menos de dos participantes en la liga");
+    }
+
+    @Test
+    void generarCalendarioLigaSinTorneo() {
+        assertThrows(IllegalStateException.class, () ->
+                torneoGestion.generarCalendarioLiga(),
+                "Se intentó crear un calendario sin un torneo");
     }
 }
