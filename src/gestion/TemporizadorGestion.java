2090e9553136f8d6cb1fde58e94aa694e1b627a7
soulsilver142 <joelrojas2025@udec.cl>
2026-07-07 00:08:05 -0400

    feat: agregar método en TemporizadorGestion para registrar el tiempo de llegada de un participante

--- src/gestion/TemporizadorGestion.java
+++ src/gestion/TemporizadorGestion.java
@@ -56,4 +56,40 @@ public class TemporizadorGestion {
         }
         return false;
     }
+
+    public Participante encontrarParticipantePorTecla(char tecla){
+        char teclaMayuscula = Character.toUpperCase(tecla);
+        for (AsignacionTecla pa : participantesConTecla){
+            if (pa.getTeclaAsignacionTecla() == teclaMayuscula){
+                return pa.getParticipanteAsignacionTecla();
+            }
+        }
+        return null;
+    }
+
+    public void registrarTiempoLlegada(char tecla){
+        if (!estaCorriendo){
+            throw new IllegalStateException("La carrera no empezó aún");
+        }
+
+        /** Se crea un participante local para el método */
+        Participante participanteLocal = encontrarParticipantePorTecla(tecla);
+        if (participanteLocal == null){
+            return;
+        }
+        if (jugadorLlegoMeta(participanteLocal)){
+            return;
+        }
+
+        long tiempoFinal = System.nanoTime() - tiempoInicio;
+
+        /** Se registra el tiempo en la lista de tiempos de llegada */
+        ParticipanteTiempoLlegada participanteRegistrado = new
+                ParticipanteTiempoLlegada(participanteLocal, tiempoFinal);
+        participantesTiemposLlegada.add(participanteRegistrado);
+    }
+
+    // Se puede añadir opcionalmente la opción de ver los tiempos
+    // de cada participante en milisegundos, para posteriormente
+    // verse en la interfaz gráfica
 }
