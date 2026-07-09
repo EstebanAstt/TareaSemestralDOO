9e54a7836244c562ed1172fc6a46194c03eb7fc3
soulsilver142 <joelrojas2025@udec.cl>
2026-07-08 23:16:25 -0400

    docs: añadir documentación javadoc a clases de temporizador

--- src/gestion/temporizador/AsignacionTecla.java
+++ src/gestion/temporizador/AsignacionTecla.java
@@ -10,16 +10,28 @@ public class AsignacionTecla {
     private char tecla;
     private Participante participante;
 
+    /**
+     * Asigna una tecla a un participante
+     * @param tecla Tecla del teclado
+     * @param participante Participante el cual se le quiere asignar una tecla
+     */
     public AsignacionTecla(char tecla, Participante participante){
         this.tecla = tecla;
         this.participante = participante;
     }
 
-    /** Getters */
+    /**
+     * Getter de la tecla guardada en el constructor
+     * @return Variable char de la tecla
+     */
     public char getTeclaAsignacionTecla(){
         return tecla;
     }
 
+    /**
+     * Getter del participante
+     * @return Participante guardado en el constructor
+     */
     public Participante getParticipanteAsignacionTecla(){
         return participante;
     }
--- src/gestion/temporizador/ParticipanteTiempoLlegada.java
+++ src/gestion/temporizador/ParticipanteTiempoLlegada.java
@@ -9,17 +9,29 @@ public class ParticipanteTiempoLlegada {
     private Participante participante;
     private long tiempo;
 
+    /**
+     * Constructor el cual guarda el participante con su respectivo tiempo de llegada
+     * @param participante Participante que llegó a la meta
+     * @param tiempo Tiempo de llegada
+     */
     public ParticipanteTiempoLlegada(Participante participante,
                                      long tiempo){
         this.participante = participante;
         this.tiempo = tiempo;
     }
 
-    /** Getters */
+    /**
+     * Getter del participante que llegó a la meta
+     * @return Participante del tiempo de llegada
+     */
     public Participante getParticipanteTiempoLlegada(){
         return participante;
     }
 
+    /**
+     * Retorna el tiempo de llegada
+     * @return Variable long que representa el tiempo
+     */
     public long getTiempoLlegada(){
         return tiempo;
     }
